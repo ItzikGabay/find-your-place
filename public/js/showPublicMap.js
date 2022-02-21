5e34ea94c7f2945f62d6e31a15e5128afaed720f
ItzikDevio <itzikdevio@gmail.com>
2022-02-22 00:13:50 +0200

    Temp refactoring

--- public/js/showPublicMap.js
+++ public/js/showPublicMap.js
@@ -1,23 +1,21 @@
 let test = campground;
 let test2 = mapToken;
 
-debugger;
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-   container: "map", // container ID
-   style: "mapbox://styles/mapbox/streets-v11", // style URL
-   center: campground.geometry.coordinates, // starting position [lng, lat]
-   zoom: 13, // starting zoom
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 13, // starting zoom
 });
 
-map.addControl(new mapboxgl.NavigationControl())
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
-   .setLngLat(campground.geometry.coordinates)
-   .setPopup(
-      new mapboxgl.Popup({ offset: 25 })
-         .setHTML(
-            `<h3>${campground.title}</h3><p>${campground.location}</p>`
-         )
-   )
-   .addTo(map)
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
+    )
+  )
+  .addTo(map);
